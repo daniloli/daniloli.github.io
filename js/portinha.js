e0ba967c67b866db82dcdd80f48d5a4e11861b5f
daniloli <danilolelinli@gmail.com>
2019-08-22 09:11:26 -0300

    Add existing file

--- js/portinha.js
+++ js/portinha.js
@@ -3,6 +3,7 @@ var poltronas=[false,true,false,true,false,true,false,true,false];
 var imagens =document.getElementsByTagName("img");
 var x=1,y=0;
 var flag=false;
+var holder;
 function carregarPoltronas(){
     
     
@@ -11,7 +12,7 @@ function carregarPoltronas(){
             imagens[i].src = "img/closed.jpg";
             //alert(imagens[i].onclick)
         }else{
-            //imagens[i].src = "img/open.jpg";
+            imagens[i].src = "";
         }
         
         
@@ -57,40 +58,61 @@ function selecionarPoltrona(valor){
         }
     }else{
         if(flag){
+            if(imagens[valor].src.match("img/open.jpg")){
+                if (x==7){
+                
+                    imagens[valor].src="img/voosh.jpeg";
+                    holder=valor;
+                }else{
+                    imagens[valor].src="img/ole.jpeg";
+                    imagens[holder].src="img/open.jpg";
+                    holder=valor;
+                }
+                        
+                    
+                    //case 20:
+                        //alert("Parabéns cara você ganhou... feliz?");
+                
+                x++;
+                
+                    
 
-            // if(){
-                 //imagens[valor].src = "img/closed.jpg";
+                    //imagens[valor-1].length="300px;";
+                    //imagens[valor].src = "img/closed.jpg";
                  
-            // }
+            }else{
+                alert("Cadê porta? Tem porta aqui não...");
+            }
+        }else{
+            switch (x){
+                case 1:
+                    alert("Você já abriu essa porta!");
+                    
+                    break;
+                case 2:
+                    alert("Essa porta parece fechada por acaso?");
+                    
+                    break;
+                case 3:
+                    alert("Sério?...");
+                    
+                    break;
+                case 4:
+                    alert("...");
+                    
+                    break;
+                case 5:
+                    alert("Parou ne?...");
+                    break;
+                case 6:
+                    alert("Ok... Vamos brincar entao");
+                    flag=true;
+                    break;
+                //case 20:
+                    //alert("Parabéns cara você ganhou... feliz?");
+            }
+            x++;
         }
-        switch (x){
-            case 1:
-                alert("Você já abriu essa porta!");
-                
-                break;
-            case 2:
-                alert("Essa porta parece fechada por acaso?");
-                
-                break;
-            case 3:
-                alert("Sério?...");
-                
-                break;
-            case 4:
-                alert("...");
-                
-                break;
-            case 5:
-                alert("Parou ne?...");
-                break;
-            case 6:
-                alert("Ok... Vamos brincar entao");
-                flag=true;
-                break;
-            //case 20:
-                //alert("Parabéns cara você ganhou... feliz?");
-        }
-        x++;
     }
 }
 function verificarPoltronas(){
